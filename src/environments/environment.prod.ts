741ad57b098cdaa4cb317e799d7c42a2755a43b0
sahil kashetwar <sahil kashetwar>
2018-04-03 15:35:49 +0530

    removing secrete keys

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
   production: true,
   config: {
     firebase: {
-      apiKey: "AIzaSyCS1lJfEEVNWKQfp-VfHKgRMpexZTy-Dq0",
-      authDomain: "workout-plan-12.firebaseapp.com",
-      databaseURL: "https://workout-plan-12.firebaseio.com",
-      projectId: "workout-plan-12",
-      storageBucket: "workout-plan-12.appspot.com",
-      messagingSenderId: "154805823244"
+      apiKey: "<api_key>",
+      authDomain: "<authdomain>",
+      databaseURL: "<db url>",
+      projectId: "<projectid>",
+      storageBucket: "<storage bucket>",
+      messagingSenderId: "<sender id>"
     }
   }
 };
